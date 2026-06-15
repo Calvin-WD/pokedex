55255d029a3f6a3d760f4865256afefb967304a4
Calvin-WD <calvinlamel@outlook.de>
2026-06-15 09:09:36 +0200

    feat: add initial Bootstrap modal structure for card extension

--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,16 @@
-function getSmallPokemonCardTemplate(pokemon, typesArray, footerHtmlString) {
-  return `<div class="card text-bg-dark smallCard-width h-100">
+function getPokemonCardTemplate(tag, pokemon, typesArray, footerHtmlString, extension = "") {
+  return `<${tag} class="card text-bg-dark card-width h-100 p-0" data-pokemon-id="${pokemon.id}" data-bs-toggle="modal" data-bs-target="#cardExtension">
       <div class="card-header d-flex flex-row-reverse justify-content-between align-items-center">
         <h5 class="text-end">${pokemon.name.toUpperCase()}</h5> <h6>#${pokemon.id}</h6>
       </div>
-      <div class="card-body position-relative d-flex justify-content-center smallCard-body-height bg-type-${typesArray[0].name} p-0">
-        <img src="${pokemon.image}" class="smallCard-img w-75" data-id="card-image">
+      <div class="card-body position-relative d-flex justify-content-center card-body-height bg-type-${typesArray[0].name} p-0">
+        <img src="${pokemon.image}" class="card-image w-75" data-id="card-image">
       </div>
-      <div class="card-footer d-flex justify-content-center gap-5">${footerHtmlString}</div>
-    </div>`;
+      <div class="d-flex justify-content-center gap-5 p-1">${footerHtmlString}</div>
+      ${extension}
+    </${tag}>`;
 }
 
-function getSmallPokemonCardTypeImageTemplate(typeImgUrl) {
-  return `<img src="${typeImgUrl}" class="smallCard-type-img">`;
+function getPokemonCardTypeImageTemplate(typeImgUrl) {
+  return `<img src="${typeImgUrl}" class="card-type-image">`;
 }
